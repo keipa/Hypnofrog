1ef8eb2708bc57edb079ca8eafc34912d87f85ed
Дима <@->
2016-07-28 20:53:08 +0200

    CodeFirst

---
+++ Hypnofrog/DBModels/Avatar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hypnofrog.DBModels
+{
+    public class Avatar
+    {
+        public int AvatarId { get; set; }
+        public string Path { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ Hypnofrog/DBModels/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hypnofrog.DBModels
+{
+    public class Context: DbContext
+    {
+        public DbSet<Avatar> Avatars { get; set; }
+        public Context() : base("DefaultConnection") { }
+    }
+}
