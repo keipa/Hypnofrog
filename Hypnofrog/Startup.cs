085b9b7ac888c8b2fa02d1f65e1fe1d68662eda0
Nickolai Rovdo <nichogaus@gmail.com>
2016-07-28 13:37:55 +0300

    Add project files.

---
+++ Hypnofrog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hypnofrog.Startup))]
+namespace Hypnofrog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
