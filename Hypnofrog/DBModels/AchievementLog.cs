c12bb66854e792daf60a004ba31ea2f89e2fa6b7
Nickolai Rovdo <nichogaus@gmail.com>
2016-08-05 17:18:45 +0300

    achievmentlog + dbset todo conditions + hall of fame + connection

---
+++ Hypnofrog/DBModels/AchievementLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hypnofrog.DBModels
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
